c219f0aac5a644d5e729d1c32260a657a8c25f1c
Fer Hernandez <fh.1111@gmail.com>
2018-12-01 23:35:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
   # code goes here
+
+  @@all = []
+  attr_reader :name, :species
+
+  def initialize(name)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @name = name
+    @species = "human"
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.say_species
+    puts "I am a #{@species}."
+  end
+
 end
